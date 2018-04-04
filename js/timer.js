c2de81e99a9475351e7a0bd4170b9599d41915dc
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2018-04-04 17:48:08 +0300

    Simple working version. Cannot yet cancel the timer, and it doesn't count how many blocks have been run

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,7 @@
+var timer = {};
+
+timer.running = false;
+
 document.addEventListener('DOMContentLoaded', function () {
   if (!Notification) {
     alert('Desktop notifications not available in your browser. Try Chromium.');
@@ -8,9 +12,42 @@ document.addEventListener('DOMContentLoaded', function () {
     Notification.requestPermission();
 });
 
+document.getElementById("start").addEventListener("click", function(){
+    var minutes = 0;
+    var seconds = 5;
+    updateScreen(minutes, 5);
+    startTimer(minutes, 5);
+});
 
-function startTimer(minutes) {
-    
+function startTimer(minutes, seconds) {
+    timer.running = true;
+    timer.minutes = minutes;
+    timer.seconds = seconds;
+    timer.intervalId = setInterval(runTimer, 1000);
+}
+
+function runTimer() {
+    if (timer.seconds > 0) {
+        timer.seconds--;
+    } else {
+        timer.minutes--;
+        timer.seconds = 59;
+    }
+    updateScreen(timer.minutes, timer.seconds);
+    checkIfTimeHasExpired();
+}
+
+function updateScreen(minutes, seconds) {
+    document.getElementById("minutes").innerHTML = String(minutes).padStart(2,0);
+    document.getElementById("seconds").innerHTML = String(seconds).padStart(2,0);
+}
+
+function checkIfTimeHasExpired() {
+    if (timer.minutes <= 0 && timer.seconds <= 0) {
+        clearInterval(timer.intervalId);
+        notifyMe('You can now take a break.', 5000);
+        timer.running = false;
+    }
 }
 
 function notifyMe(message, timeout) {
